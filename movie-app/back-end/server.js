f239bf12002f9f302c9264e0382ea08322f252cf
cryyeds <cryyeds@gmail.com>
2026-07-21 11:10:20 -0500

    Replace repository with new project

--- movie-app/back-end/server.js
+++ movie-app/back-end/server.js
@@ -17,7 +17,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 app.use(express.json());
 
 const TMDB_BASE_URL = "https://api.themoviedb.org/3";
